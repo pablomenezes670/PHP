498d4f208c5f5a74bb7243baddeae86ae1f5c7f7
Pablo Menezes <pablomenezes670@users.noreply.github.com>
2017-04-25 16:18:03 -0300

    Add files via upload

---
+++ EXEMPLO 11.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+<h1>Estrutura de Repetição(Do While)</h1>
+<?php
+$num=10;
+do{
+echo "numero:$num<br>";
+$num--;	
+}while($num>=1)
+?>
+</body>
+</html>
