806da4895076d5bba8c55d8a437498d404873950
Pablo Menezes <pablomenezes670@users.noreply.github.com>
2017-04-25 15:56:19 -0300

    Add files via upload

---
+++ EXEMPLO 10.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+<h1>estrutura de repetição(While)</h1>
+<?php
+$num=1;
+while($num<=10){
+echo"numero:$num<br>";
+$num++;	
+}
+?>
+</body>
+</html>
