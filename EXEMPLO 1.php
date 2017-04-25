5822727b7296c01e1df4b617592c7da4ddaa9efd
Pablo Menezes <pablomenezes670@users.noreply.github.com>
2017-04-25 16:13:56 -0300

    Add files via upload

---
+++ EXEMPLO 1.php
@@ -0,0 +1,13 @@
+<HTML>
+<HEAD>
+ <meta charset-"utf-8"/>
+ <TITLE>EXEMPLO 1</TITLE>
+</HEAD>
+<BODY>
+<?php
+  $idade=16;
+  $nome="aluno do Marly";
+  echo " Olá meu nome é:", $nome, "<br>Minha idade é:",$idade;
+?>
+</BODY>
+</HTML>
---
+++ EXEMPLO 2.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+ <title>EXEMPLO 2</title>
+</head>
+<BODY>
+<?php
+ echo "*****constante declaradas*****<br>";
+  define("CONSTANTE_ALO", " Olá turma! assim, declara uma costante");
+  echo CONSTANTE_ALO;
+  echo "<br><br><br>*****constante predefinidas*****<br>";
+  echo phpinfo();
+?>
+</body>
+</html>
