e8780348c088d9c764df5e4801867da84790a074
Pablo Menezes <pablomenezes670@users.noreply.github.com>
2017-05-07 12:37:09 -0300

    Rename EXEMPLO 15.php.txt to EXEMPLO 15.php

--- EXEMPLO 15.php
+++ EXEMPLO 15.php
@@ -8,10 +8,10 @@
 function mais5($numero){
 $numero += 5;
 }
-echo"<h1>Passagem de par‚metro</h1>";
+echo"<h1>Passagem de par√¢metro</h1>";
 $a=3;
 mais5($a);
-echo"Valor Vari·vel a = $a <br>";
+echo"Valor Vari√°vel a = $a <br>";
 ?>
 
 </body>
